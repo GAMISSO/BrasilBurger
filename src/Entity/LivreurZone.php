ff96998c2cc773c0fc1a82d4db5495e3ea2f3a44
GAMISSO <koffi-emmanuel.gamisso@ism.edu.sn>
2025-12-30 15:30:35 +0100

    fix: Convert LivreurZone to composite key with ManyToOne relationships

--- src/Entity/LivreurZone.php
+++ src/Entity/LivreurZone.php
@@ -8,27 +8,31 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Table(name: 'livreur_zone')]
 class LivreurZone
 {
-    #[ORM\Column(type: 'integer')]
-    private int $livreur_id;
+    #[ORM\Id]
+    #[ORM\ManyToOne(targetEntity: Livreur::class)]
+    #[ORM\JoinColumn(name: 'livreur_id', referencedColumnName: 'id')]
+    private Livreur $livreur;
 
-    public function getLivreur_id(): int {
-        return $this->livreur_id;
+    #[ORM\Id]
+    #[ORM\ManyToOne(targetEntity: Zones::class)]
+    #[ORM\JoinColumn(name: 'zone_id', referencedColumnName: 'id')]
+    private Zones $zone;
+
+    public function getLivreur(): Livreur {
+        return $this->livreur;
     }
 
-    public function setLivreur_id(int $livreur_id): self {
-        $this->livreur_id = $livreur_id;
+    public function setLivreur(Livreur $livreur): self {
+        $this->livreur = $livreur;
         return $this;
     }
 
-    #[ORM\Column(type: 'integer')]
-    private int $zone_id;
-
-    public function getZone_id(): int {
-        return $this->zone_id;
+    public function getZone(): Zones {
+        return $this->zone;
     }
 
-    public function setZone_id(int $zone_id): self {
-        $this->zone_id = $zone_id;
+    public function setZone(Zones $zone): self {
+        $this->zone = $zone;
         return $this;
     }
 
