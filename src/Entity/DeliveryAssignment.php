8032e09923a3f9a7178a88d44fc0a6edba5b8bf9
GAMISSO <koffi-emmanuel.gamisso@ism.edu.sn>
2025-12-30 12:34:29 +0100

    fix: Add primary key annotations to DeliveryAssignment entity

--- src/Entity/DeliveryAssignment.php
+++ src/Entity/DeliveryAssignment.php
@@ -8,6 +8,8 @@ use Doctrine\ORM\Mapping as ORM;
 #[ORM\Table(name: 'delivery_assignment')]
 class DeliveryAssignment
 {
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
     #[ORM\Column(type: 'integer')]
     private int $id;
 
