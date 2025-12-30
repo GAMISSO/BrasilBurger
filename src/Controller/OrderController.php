eb05380bcbc6ed8e634f21f36f784e3d4bfcd88e
GAMISSO <koffi-emmanuel.gamisso@ism.edu.sn>
2025-12-30 19:18:51 +0100

    fix: Add OrderService and PaiementService aliases, use interface in OrderController

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -8,7 +8,7 @@ use App\Service\CommandeService;
 use App\DTO\CommandeFilterDTO;
 use App\DTO\OrderFilterDTO;
 use App\Form\OrderFilterType;
-use App\Service\Impl\OrderServiceImpl;
+use App\Service\OrderService;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
@@ -20,7 +20,7 @@ use Symfony\Component\Routing\Attribute\Route;
 class OrderController extends AbstractController
 {
     public function __construct(
-        private OrderServiceImpl $commandeService,
+        private OrderService $commandeService,
         private EntityManagerInterface $entityManager
     ) {}
 
--- src/Service/OrderService.php
+++ src/Service/OrderService.php
@@ -10,6 +10,7 @@ interface OrderService
     public function creerCommande(array $data): OrderTable;
     public function ajouterLigneCommande(OrderTable $commande, array $itemData): OrderLine;
     public function changerEtat(OrderTable $commande, string $nouvelEtat): void;
+     public function annulerCommande(OrderTable $commande): void;
     public function terminerCommande(OrderTable $commande): void;
     public function peutEtreAnnulee(OrderTable $commande): bool;
     public function calculerMontantTotal(OrderTable $commande): int;
