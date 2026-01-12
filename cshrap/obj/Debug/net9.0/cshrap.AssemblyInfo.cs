3cd5952f25234a21857d6c2ceb4d48219f0a4dd1
GAMISSO <koffi-emmanuel.gamisso@ism.edu.sn>
2026-01-12 15:35:18 +0100

    new reposi nine

--- cshrap/obj/Debug/net9.0/cshrap.AssemblyInfo.cs
+++ cshrap/obj/Debug/net9.0/cshrap.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("cshrap")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8c919fdf3d1ee2295a8ad23d122102e11d2b8b4d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f781596bd47f51f3846822d88f165c2cfac6c488")]
 [assembly: System.Reflection.AssemblyProductAttribute("cshrap")]
 [assembly: System.Reflection.AssemblyTitleAttribute("cshrap")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
